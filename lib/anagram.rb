7ed2b1953a3d901d99611c3014fc13c788b00576
Malcom Fetus <sora.vista@gmail.com>
2020-05-26 21:21:45 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -17,8 +17,9 @@ def match(array)
   array.keep_if do |string|
     #keep_if is a method that removes anything false and keep only elements that are true
     #binding.pry
-
     name.split("").sort == string.split("").sort
+    #<- name here is being split and then being sorted and checking if it is equal to string element also being splitted and sorted
+    #<- once its false, keep_if will remove both of them and only keep the elements that are true.
   end
 end
 
