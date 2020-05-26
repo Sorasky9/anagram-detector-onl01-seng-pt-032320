3ccd19872afcda52c9ce781149ec9f8ed5b22b24
Malcom Fetus <sora.vista@gmail.com>
2020-05-26 21:13:08 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_reader :name
+
+def initialize(name)
+  @name = name
+  #binding.pry
+end
+
+# def match(array)
+#   match = []
+#   #binding.pry
+# end
+
+def match(array)
+  array.keep_if do |string|
+    #binding.pry
+
+    name.split("").sort == string.split("").sort
+  end
+end
+
+
+
+
+end
